0a62f3b00c9268fc48e8a0f891e8395beaf344d6
Sam Elder <samelder925@gmail.com>
2022-06-30 20:09:05 -0400

    part 1 finish, except ending prompt

--- part-1.js
+++ part-1.js
@@ -2,7 +2,6 @@
 
 var rs = require('readline-sync');
 
-
 const arrayA = ['A1', 'A2', 'A3'];
 const arrayB = ['B1', 'B2', 'B3'];
 const arrayC = ['C1', 'C2', 'C3'];
@@ -12,16 +11,26 @@ const nestedArray = [arrayA, arrayB, arrayC];
 const shipOne = 'S1';
 const shipTwo = 'S2';
 
-function removeReplace (array, index, newVar) {
-    array.splice(index, 1, newVar);
-    return index;
-}
+let shipOneCoord = '';
+let shipTwoCoord = '';
+
+let guess='';
+
+guessType = nestedArray;
+
+const guessArray=[];
+
+// USE for in PART 3 GUI
+// function removeReplace (array, index, newVar) {
+//     array.splice(index, 1, newVar);
+//     return index;
+// }
 
 // generates random whole number between 0 and argument
 function random(gridSize) {
     return Math.floor(Math.random()* gridSize);
 }
-
+// generates random coords
 function randomGridCoord() {
     let letter = random(3)
 
@@ -33,22 +42,43 @@ function randomGridCoord() {
     
 }
 
-// places ships randomly in arrays
+// places ships randomly in arrays & checks for overlap
 function placeShips () {
-    const shipOneCoord = randomGridCoord();
-    let shipTwoCoord = randomGridCoord();
+    shipOneCoord = randomGridCoord();
+    shipTwoCoord = randomGridCoord();
 
-    if (shipTwoCoord === shipOneCoord) {
+    while (shipTwoCoord === shipOneCoord) {
         shipTwoCoord = randomGridCoord();
     };
 
-
-    console.log('\n*Hidden* Ship 1 : '+ shipOneCoord);
-    console.log('*Hidden* Ship 2 : '+ shipTwoCoord + '\n');
+// This will be hidden to the player
+    console.log('\n*HIDE* Ship 1 : '+ shipOneCoord);
+    console.log('*HIDE* Ship 2 : '+ shipTwoCoord + '\n');
 }
 
+
+
+function checkGuess (guess) {
+   
+
+    if (guessArray.includes(shipOneCoord)  && guess === shipTwoCoord) {
+        console.log('You have destroyed all battleships!')
+    }
+    else if (guessArray.includes(shipTwoCoord)  && guess === shipOneCoord) {
+        console.log('You have destroyed all battleships!')
+    }
+    else if (guessArray.includes(guess) ) {
+        console.log('You have already picked this location. Miss!')
+    }
+    else if (guess === shipOneCoord || guess === shipTwoCoord) {
+        console.log('HIT! You have sunk a battleship. 1 ship remaining')
+    }
+    else console.log('You have missed!');
+}
+
+
 // initiate game
-operation = rs.question('Press any key to start the game.');
+rs.question('Press any key to start the game.');
 
 placeShips();
 
@@ -56,6 +86,23 @@ console.log(arrayA);
 console.log(arrayB);
 console.log(arrayC);
 
-// rs.question( '\n Enter a location to strike. ie "A2"...');
+
+
+while (!(guessArray.includes(shipOneCoord) && guessArray.includes(shipTwoCoord))) {
+
+guess = rs.question( '\n Enter a location to strike. ie "A2"...\n', {limit: guessType,
+        limitMessage:'That is not a valid location. Please try again.'});
+
+        
+checkGuess(guess);
+        
+guessArray.push(guess);
+
+console.log('Your Strikes:' + guessArray);}
+
+
+if (guessArray.includes(shipOneCoord) && guessArray.includes(shipTwoCoord)) 
+{rs.keyInYN('Would you like to play again? Y/N')}
+
 
 
